3a26eb8cf677844324cd9c385a4d0d0bd6f00891
Leesungsup <tmdtjq0116@naver.com>
2022-07-30 23:08:43 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
+
+const io = require('socket.io')(server)
+
+io.sockets.on('connection', (socket) => {
+    console.log(`Socket connected : ${socket.id}`)
+
+    socket.on('disconnect', () => {
+        console.log(`Socket disconnected : ${socket.id}`)
+    })
+})
+
+server.listen(80, () => {
+    console.log(`Server listening at http://localhost:80`)
+})
