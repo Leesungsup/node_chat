b6a54fca12f7cefb568b5ad9afec456855fca5d6
Leesungsup <tmdtjq0116@naver.com>
2022-08-01 00:59:48 +0900

    socket

--- app.js
+++ app.js
@@ -3,16 +3,57 @@ const http = require('http')
 const app = express()
 const server = http.createServer(app)
 
-const io = require('socket.io')(server)
+const socketIo = require("socket.io");
+const io = socketIo(server);
+/*app.get('/', function(req,res) {
+    res.sendFile(__dirname + "/public/client.html")
+})*/
 
 io.sockets.on('connection', (socket) => {
-    console.log(`Socket connected : ${socket.id}`)
+    console.log(`Socket connected : ${socket}`)
+
+    socket.on('subscribe', (data) => {
+        const roomData = JSON.parse(data)
+        const username = roomData.username
+        const roomNumber = roomData.roomNumber
+
+        socket.join(`${roomNumber}`)
+        console.log(`[Username : ${username}] entered [room number : ${roomNumber}]`)
+
+        const subData = {
+            type : "ENTER",
+            content : `${username} entered the room`
+        }
+        socket.broadcast.to(`${roomNumber}`).emit('update', JSON.stringify(subData))
+    })
+
+    socket.on('unsubscribe', (data) => {
+        const roomData = JSON.parse(data)
+        const username = roomData.username
+        const roomNumber = roomData.roomNumber
+
+        socket.leave(`${roomNumber}`)
+        console.log(`[Username : ${username}] left [room number : ${roomNumber}]`)
+
+        const unsubData = {
+            type : "LEFT",
+            content : `${username} left the room`
+        }
+        socket.broadcast.to(`${roomNumber}`).emit('update', JSON.stringify(unsubData))
+    })
+
+    socket.on('newMessage', (data) => {
+        const messageData = JSON.parse(data)
+        console.log(`[Room Number ${messageData.to}] ${messageData.from} : ${messageData.content}`)
+        io.to(`${messageData.to}`).emit('update', JSON.stringify(messageData))
+    })
 
     socket.on('disconnect', () => {
         console.log(`Socket disconnected : ${socket.id}`)
     })
 })
 
-server.listen(80, () => {
-    console.log(`Server listening at http://localhost:80`)
+
+server.listen(8080, () => {
+    console.log(`Server listening at http://localhost:8080`)
 })
