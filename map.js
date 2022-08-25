cde7c450a59be433e865a40ecb06d263f11c7969
Leesungsup <tmdtjq0116@naver.com>
2022-08-25 23:25:55 +0900

    axios

--- map.js
+++ map.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3003
+var axios = require("axios");
+var port = process.env.PORT || 3003;
 app.use(express.static('public_html'));
 
 app.listen(port,function(){
     console.log("서버시작");
+});
+
+app.get("/pharmach_list",(req,res)=>{
+    let response=null;
+    try {
+        let api = async () => {
+            response = await axios.get("");
+        }
+        api.then((response)=>{
+            res.setHeader("Acess-Control-Allow-Origin","*");
+            res.json(response.data,response.body);
+        });
+    }
+    catch (e){
+        console.log(e);
+    }
+    return response;
 });
